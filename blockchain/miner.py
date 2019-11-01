3bee4b22a9edaead4760eed26045d3ab153a0b4c
GianBarGian <giacomobenati@hotmail.it>
2019-11-01 09:39:45 +0100

    miner up and running

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,8 +24,12 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
-    #  TODO: Your code here
+    proof = random.randint(100000, 1000000)
+    encoded = str(last_proof).encode()
+   
+    last_hash = hashlib.sha256(encoded).hexdigest()
+    while valid_proof(last_hash, proof) is False:
+        proof +=1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -38,10 +42,9 @@ def valid_proof(last_hash, proof):
 
     IE:  last_hash: ...AE9123456, new hash 123456888...
     """
-
-    # TODO: Your code here!
-    pass
-
+    encoded = str(proof).encode()
+    guess_hash = hashlib.sha256(encoded).hexdigest()
+    return last_hash[-6:] == guess_hash[:6]
 
 if __name__ == '__main__':
     # What node are we interacting with?
@@ -66,6 +69,7 @@ if __name__ == '__main__':
         # Get the last proof from the server
         r = requests.get(url=node + "/last_proof")
         data = r.json()
+
         new_proof = proof_of_work(data.get('proof'))
 
         post_data = {"proof": new_proof,
@@ -73,6 +77,7 @@ if __name__ == '__main__':
 
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
+        print(data)
         if data.get('message') == 'New Block Forged':
             coins_mined += 1
             print("Total coins mined: " + str(coins_mined))
